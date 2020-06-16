4c4bbad9b05ae87c3f508846d56102a0a73e8914
SondosSamii <sondos.sami.abuzeid@gmail.com>
2020-06-16 21:45:29 +0200

    Edit

--- js/index.js
+++ js/index.js
@@ -1,16 +1,26 @@
-$(document).ready(function() {
-    $(".navbar-nav li").click(function() {
+$(document).ready(function () {
+    $(".navbar-nav li").click(function () {
         $(this).addClass("current");
         $(".navbar-nav li").not($(this)).removeClass("current");
     });
 
-    $('.counter h3').each(function() {
+    // window.addEventListener('scroll', function (e) {
+    //     var scroll = this.window.scrollY;
+    //     console.log(scroll);
+    //     if (scroll == 600) {
+    //         console.log(true);
+    //         $(".navbar-nav:nth-child(2)").addClass("current");
+    //         $(".navbar-nav:nth-child(2)").not($(this)).removeClass("current");
+    //     }
+    // });
+
+    $('.counter h3').each(function () {
         $(this).prop('Counter', 0).animate({
             Counter: $(this).text()
         }, {
             duration: 4000,
             easing: 'swing',
-            step: function(now) {
+            step: function (now) {
                 $(this).text(Math.ceil(now));
             }
         });
