e050cc2d9c790971e3e0e1d8cf4a35d205968ec7
SondosSamii <sondos.sami.abuzeid@gmail.com>
2020-06-21 17:15:32 +0200

    Add DOMContentLoaded as an  Event Listener

--- js/index.js
+++ js/index.js
@@ -1,28 +1,4 @@
-$(document).ready(function () {
-    function toggleCurrentClass() {
-        const lis = document.querySelectorAll('.navbar li');
-        // console.log(lis);
-
-        // console.log(window.scrollY);
-        for (let i = 0; i < lis.length; i++) {
-            lis[i].classList.remove("current");
-            if (window.scrollY >= 0 && window.scrollY < 614) {
-                lis[0].classList.add("current");
-            }
-            else if (window.scrollY >= 614 && window.scrollY < 1800) {
-                lis[1].classList.add("current");
-            }
-            else if (window.scrollY >= 1800 && window.scrollY < 3015) {
-                lis[2].classList.add("current");
-            }
-            else if (window.scrollY >= 3015 && window.scrollY < 4810) {
-                lis[3].classList.add("current");
-            }
-            else if (window.scrollY >= 4810) {
-                lis[4].classList.add("current");
-            }
-        }
-    }
+window.addEventListener('DOMContentLoaded', function () {
     window.addEventListener('scroll', toggleCurrentClass, true);
 
     window.removeEventListener('scroll', toggleCurrentClass, false);
@@ -38,4 +14,29 @@ $(document).ready(function () {
             }
         });
     });
-});
+}, true);
+
+function toggleCurrentClass() {
+    const lis = document.querySelectorAll('.navbar li');
+    // console.log(lis);
+
+    // console.log(window.scrollY);
+    for (let i = 0; i < lis.length; i++) {
+        lis[i].classList.remove("current");
+        if (window.scrollY >= 0 && window.scrollY < 614) {
+            lis[0].classList.add("current");
+        }
+        else if (window.scrollY >= 614 && window.scrollY < 1800) {
+            lis[1].classList.add("current");
+        }
+        else if (window.scrollY >= 1800 && window.scrollY < 3015) {
+            lis[2].classList.add("current");
+        }
+        else if (window.scrollY >= 3015 && window.scrollY < 4810) {
+            lis[3].classList.add("current");
+        }
+        else if (window.scrollY >= 4810) {
+            lis[4].classList.add("current");
+        }
+    }
+}
